5a1acc81616c2dba477a6fc373c07c5b96004611
kodal1993 <kodal1993@gmail.com>
2026-05-25 08:16:37 +0200

    Add recenter cooldown config

--- src/config.py
+++ src/config.py
@@ -69,6 +69,8 @@ class BotConfig:
     dust_position_notional_usd: float
     no_fill_cycles_before_recenter: int
     grid_stale_max_hours: float
+    recenter_cooldown_seconds: int
+    position_recenter_cooldown_seconds: int
 
     @classmethod
     def from_env(cls) -> "BotConfig":
@@ -152,4 +154,6 @@ class BotConfig:
             dust_position_notional_usd=float(os.getenv("DUST_POSITION_NOTIONAL_USD", "1")),
             no_fill_cycles_before_recenter=int(os.getenv("NO_FILL_CYCLES_BEFORE_RECENTER", "360")),
             grid_stale_max_hours=float(os.getenv("GRID_STALE_MAX_HOURS", "2")),
+            recenter_cooldown_seconds=int(os.getenv("RECENTER_COOLDOWN_SECONDS", "600")),
+            position_recenter_cooldown_seconds=int(os.getenv("POSITION_RECENTER_COOLDOWN_SECONDS", "900")),
         )
